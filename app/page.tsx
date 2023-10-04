7b4095610e3c90d0e1f3adc83234139bf83793c2
testeProgramador21 <146689338+testeProgramador21@users.noreply.github.com>
2023-10-04 05:06:07 -0300

    pronto

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 const Page = () => {
   return (
     <div className=" container mx-auto">
-
+      <div className="group bg-blue-100 p-3 dark:bg-blue-800 hover:bg-green-700">
+        <p className="text-black dark:text-white group-hover:text-white">Texto 1</p>
+        <p className="text-gray-500  dark:text-white group-hover:text-white">Texto 2</p>
+      </div>
 
       
     </div>
